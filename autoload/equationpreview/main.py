1d05e0ac6cad1461eeb0a48acb4971934c78734c
Takuro Fujino <fujino.takuro.92@gmail.com>
2024-04-08 20:07:31 +0900

    Fix bugs and typo.

--- autoload/equationpreview/main.py
+++ autoload/equationpreview/main.py
@@ -4,15 +4,16 @@ dpi = plt.rcParams['figure.dpi']
 eq_list = %s
 width = %d
 color = '%s'
-fontsize = %d
+fontsize = %d  # pt
 L = len(eq_list)
-height = fontsize*L*2.0+0.2
+h = fontsize/72.0*L*2.0+0.2  # inch
 
-h = float(height)/dpi
-w = float(width)/dpi
+w = float(width)/dpi  # inch
 fig = plt.figure(figsize=(w, h))
 ax1 = fig.add_axes([0.0, 0.0, 1.0, 1.0])
 for i, eq_str in enumerate(eq_list):
-    ax1.text(0.2, 0.1+(1.0/L*(L-1-i)), r'${}$'.format(eq_str), fontdict=dict(color=color, fontsize=fontsize))
+    ax1.text(0.2, 0.1+(1.0/L*(L-1-i)), r'${}$'.format(eq_str),
+             fontdict=dict(color=color, fontsize=fontsize,
+                           va='bottom', ha='left'))
 ax1.grid(False)
 plt.show()
